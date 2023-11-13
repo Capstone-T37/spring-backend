7e2d6fc424b714a20e2bf8bd7a8ac9e2a271bba3
ayman fakri <61848289+afakri@users.noreply.github.com>
2023-11-13 18:19:35 -0500

    fixed tests

--- src/main/java/com/mycompany/myapp/web/rest/PublicUserResource.java
+++ src/main/java/com/mycompany/myapp/web/rest/PublicUserResource.java
@@ -45,6 +45,23 @@ public class PublicUserResource {
         if (!onlyContainsAllowedProperties(pageable)) {
             return ResponseEntity.badRequest().build();
         }
+        final Page<UserDTO> page = userService.getAllPublicUsers(pageable);
+        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
+        return new ResponseEntity<>(page.getContent(), headers, HttpStatus.OK);
+    }
+
+    /**
+     * {@code GET /users} : get all other users with only the public informations - calling this are allowed for anyone.
+     *
+     * @param pageable the pagination information.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body all users.
+     */
+    @GetMapping("/users/others")
+    public ResponseEntity<List<UserDTO>> getAllOtherPublicUsers(@org.springdoc.api.annotations.ParameterObject Pageable pageable) {
+        log.debug("REST request to get all public User names");
+        if (!onlyContainsAllowedProperties(pageable)) {
+            return ResponseEntity.badRequest().build();
+        }
 
         Optional<User> user = userService.getUserWithAuthorities();
         if (user.isEmpty()) {
